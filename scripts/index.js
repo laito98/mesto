6f6308e00cc49b37f3d07139f975be4ad6e8e457
Laura <laura.kudryavtseva@ya.ru>
2022-12-05 01:16:27 +0300

    Project 5

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //общие штуки
 //функция видимости
 const openPopup = (popup) => {
@@ -6,20 +7,20 @@ const openPopup = (popup) => {
 
 
 // объявления для формы 1
+=======
+>>>>>>> parent of 33b5db2 (POPUPS)
 const popupOpenButtonElement = document.querySelector(`.profile__edit-button`);
 const popup = document.querySelector(`.popup`);
-const popupProfile = document.querySelector(`.popup_profile`);
 const popupElement = document.querySelector(`.popup__container`);
 const popupForm = document.querySelector(`.popup__form`);
 const popupCloseElement = popupElement.querySelector(`.popup__close-button`);
-const nameInput = popupElement.querySelector(`.popup__input_field_name`);
-const descriptionInput = popupElement.querySelector(`.popup__input_field_description`);
-const popupSaveButton = popupElement.querySelector(`.popup__save-button`);
-
-// объявления для профиля
+const nameInput = document.querySelector(`.popup__input_field_name`);
+const descriptionInput = document.querySelector(`.popup__input_field_description`);
+const popupSaveButton = document.querySelector(`.popup__save-button`);
 const profileName = document.querySelector(`.profile__name`);
 const profileDescription = document.querySelector(`.profile__description`);
 
+<<<<<<< HEAD
 // функция сохранения информации профиля
 function formSubmitHandler(evt) {
   evt.preventDefault();
@@ -180,7 +181,51 @@ bigImagePopupClose.addEventListener('click', () =>{
 
 
 
+=======
 
+const addPopupVisibility = function () {
+  popup.classList.add(`popup_opened`);
+  nameInput.textContent = profileName.textContent;
+  descriptionInput.textContent = profileDescription.textContent;
+};
+
+const removePopupVisibility = function () {
+  popup.classList.remove(`popup_opened`);
+};
+
+
+function formSubmitHandler(evt){
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent= descriptionInput.value;
+  removePopupVisibility();
+};
+
+
+
+popupOpenButtonElement.addEventListener('click', addPopupVisibility);
+popupCloseElement.addEventListener('click', removePopupVisibility);
+popupForm.addEventListener(`submit`, formSubmitHandler);
+>>>>>>> parent of 33b5db2 (POPUPS)
+
+
+
+//const activateLikeButton = function(){
+//  cardLikeButton.classList.remove(`element__like-button`);
+//  cardLikeButton.classList.toggle(`element__like-button_is-activated`);
+//}
+//cardLikeButton.addEventListener(`click`, activateLikeButton);
+ // const cardElement = document.querySelector(`.element`);
+ // const cardLikeButton = cardElement.querySelector(`.element__like-button`);
+ // cardLikeButton.addEventListener(`click`, (function () {
+ //   if (cardLikeButton.classList.contains(`element__like-button`)){
+ //     cardLikeButton.classList.remove(`element__like-button`);
+ //     cardLikeButton.classList.toggle(`element__like-button_is-activated`);
+ //   } else {
+ //     cardLikeButton.classList.remove(`element__like-button_is-activated`);
+ //     cardLikeButton.classList.toggle(`element__like-button`);
+ //   }
+ // }));
 
 
 
